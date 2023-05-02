0ca77ae99a0a3396880b28b907f7f433cbb31ed0
MaryKreslin <112550019+MaryKreslin@users.noreply.github.com>
2023-05-02 13:36:00 +0300

    refact: add cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const process = require('process');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const NotFoundErr = require('./errors/notFoundErr');
 const handleErrors = require('./middlewares/handleErrors');
 const { auth } = require('./middlewares/auth');
@@ -12,7 +13,7 @@ const routes = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
